8ac300970b953cc24a1a1596457d6a3064b22a7b
Nam Lê Công <namlecong@Nams-MacBook-Pro.local>
2022-06-25 17:44:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleGetHome = () => {
+    axios.get("http://localhost:3000/home123").then((res) => {
+      console.log(res);
+    });
+  };
+
+  const handleClickButton = () => {
+    console.log(username, password);
+    axios
+      .post("http://localhost:3000/api/login", {
+        email: username,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+        // if (res.data.success === true) {
+        //   axios.post("http://localhost:5000/api/refreshToken").then((res) => {
+        //     console.log(res);
+        //   });
+        // }
+      });
+  };
+
+  const handleRefreshToken = () => {
+    console.log("refresh");
+    axios
+      .post("http://localhost:3000/api/refreshToken", {
+        username: username,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input
+        type={"password"}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClickButton}>Đăng nhập</button>
+      <button onClick={handleRefreshToken}>Refresh Token</button>
+      <button onClick={handleGetHome}>Home</button>
     </div>
   );
 }
